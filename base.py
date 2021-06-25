49195253ee23e5d0fa4bd6a6bd79d78c1f8405a5
killmy <1601519346@qq.com>
2021-06-25 17:47:21 +0800

    python learning

---
+++ base.py
@@ -0,0 +1,20 @@
+'''
+coding:utf-8
+name:gfh
+using: python basic knowledge
+'''
+# 任意整数
+a = 0x55 #16
+b = 10_000_000 #很大的数可以分开写
+c = 0xff_123
+Print_out = "a is:"+str(a)+" b is:"+str(b)
+print(Print_out)
+
+# 浮点数
+a = 1.23e9 #科学计数法 1.23x10^9
+b = 1.23e-9 #1.23x10^-9
+c = 12/5 #精确计算
+d = 12/5.1
+e = 12//5#整除
+Print_out = str(a)+" "+str(b)+" "+str(c)+" "+str(d)+" "+str(e)
+print(Print_out)
---
+++ gogle命名规范.py
@@ -0,0 +1,5 @@
+'''
+coding:utf-8
+name:gfh
+using:学习gogle的开源python规范以及命名规范
+'''
