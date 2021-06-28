0254553a81bb9e9e2c513a53461d924f2ed3913f
killmy <1601519346@qq.com>
2021-06-28 13:22:46 +0800

    python learning

--- base.py
+++ base.py
@@ -18,3 +18,15 @@ d = 12/5.1
 e = 12//5#整除
 Print_out = str(a)+" "+str(b)+" "+str(c)+" "+str(d)+" "+str(e)
 print(Print_out)
+
+#字符串
+a = 'abc'
+b = 'a\"tt\"' #\ 转义字符
+c = r'a"b"' #默认不转义
+d = r'a\"b"'
+e = '\\\t\\\n\\\t\\'#\\-->\ \n换行
+f = '''line1
+... line2
+... line3
+'''
+print(a+"\n"+b+"\n"+c+"\n"+d+"\n"+e+"\n"+f+"\n")
