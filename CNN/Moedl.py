6ff4489dbcfde16a94a740954b2fa1eb61c738c3
killmy <1601519346@qq.com>
2021-07-16 21:17:57 +0800

    feedforward

---
+++ CNN/Moedl.py
@@ -0,0 +1,37 @@
+'''
+coding:utf-8
+网站:https://github.com/yunjey/pytorch-tutorial/blob/master/tutorials/02-intermediate/convolutional_neural_network/main.py#L35-L56
+https://zhuanlan.zhihu.com/p/220403674
+'''
+import torch
+import torch.nn as nn
+import hiddenlayer as h
+from torch.nn.modules.batchnorm import BatchNorm2d
+
+# 在cnn中一张图片看成一个数据
+class MNIST_CNNModel(nn.Module):
+    def __init__(self,num_classes=10):
+        super(MNIST_CNNModel,self).__init__()
+        self.layer1 = nn.Sequential(
+            #(image.size+2*padding-kernel_size)+stride
+            nn.Conv2d(1,16,kernel_size=5,stride=1,padding=2),
+            #对应通道对应数据做，Batch_sizexWxH
+            nn.BatchNorm2d(16),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2,stride=2))
+        self.layer2 = nn.Sequential(
+            nn.Conv2d(16,32,kernel_size=5,stride=1,padding=2),
+            nn.BatchNorm2d(32),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2,stride=2)
+        )
+        self.fc = nn.Linear(7*7*32,num_classes)
+    def forward(self,x):
+        out = self.layer1(x)
+        out = self.layer2(out)
+        out = out.reshape(out.size(0),-1)
+        out = self.fc(out)
+        return out
+
+model = MNIST_CNNModel(10)
+vis_grap = h.build_graph(model,torch.randn(1,28,28))
---
+++ CNN/readme.md
@@ -0,0 +1,7 @@
+### MNIST 手写字体的识别
+1. hidden layer
+卷积->batch normal->relu->下采样
+2. reshape
+3. fully connect->num_classess 
+
+交叉熵自带softmax
